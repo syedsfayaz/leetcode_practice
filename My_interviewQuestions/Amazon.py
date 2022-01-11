9baaa074a06ae51bb8c55ee4d8c0839c4f3197e4
Fayaz Sayyed <Fayaz_Sayyed@na.honda.com>
2022-01-10 19:11:23 -0800

    added new file

---
+++ My_interviewQuestions/Amazon.py
@@ -0,0 +1,26 @@
+Find the index in an array of ints where the difference between sum on the left and the sum on the right is minimal.
+#
+#Example:
+#
+#[ 2, 3, 8, 1] ## answer is 2 -- 2 + 3 = 5, 8 +1 =9 
+
+
+def min_index(input):
+    if len(input) == 0:
+        return 0
+    if len(input) == 1:
+        return 1
+        
+    result = {}
+    
+    for index in range(len(input)):
+        left_list = input[:index]
+        right_list = input[index:]
+        
+        difference = abs(sum(right_list) - sum(left_list))
+        
+        if difference not in result:
+                result[difference] = index
+    min_diff = min(result)            
+    return result[min_diff]
+ 
--- My_interviewQuestions/Salesforce.py
+++ My_interviewQuestions/Salesforce.py
@@ -1,9 +1,3 @@
-Monday, December 6th
-Interview Schedule:
-https://appleinc.webex.com/appleinc/j.php?MTID=m8c0d35e67f50ea87801bb099162b63b6
-Host Key: 231482
-Coderpad Link - https://app.coderpad.io/ZKMTHTCD
-2:30 p.m. to 2:45 p.m. PST with Connie Bustillo
 '''
 Write a code/function to validate the pair of parenthesis in expression. 
 Return True/False
@@ -21,37 +15,32 @@ def valid_paranthesis(input):
     if len(input) % 2 != 0:
         return False
         
-    dict_items = {"(":0}
+    dict_items = {"(": 0}
     
     for items in input:
         if items == "(":
             dict_items["("] += 1
-        elif  dict_items["("] <= 0:
-            return False
         else:
-            items["("] -= 1
+            dict_items["("] -= 1
+            if dict_items["("] < 0:
+                return False
     if dict_items["("] != 0:
         return False
     return True
 
 
-        
+print(valid_paranthesis("()()()"))
     
     
-002225
-
-
-Is this a full time role 
-How ofte you release
-what code are you building
-GIL
-over rite the default values in helm
-how to add list in dictionary
-
-
-GRE global release engi group
-
-
-
-
-
+# 002225
+#
+#
+# Is this a full time role
+# How ofte you release
+# what code are you building
+# GIL
+# over rite the default values in helm
+# how to add list in dictionary
+#
+#
+# GRE global release engi group
